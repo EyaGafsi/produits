4a2e2cf64e842fc2a26d35941864f8981e07805e
Eya Gafsi <eyagafsi17@gmail.com>
2023-04-11 10:07:11 +0100

    TP5

--- src/main/java/com/iset/produits/controllers/CatController.java
+++ src/main/java/com/iset/produits/controllers/CatController.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -24,24 +26,25 @@ public class CatController {
     @Autowired
     ProduitService produitService;
     @RequestMapping("/showCreate")
-    public String showCreate()
+    public String showCreate(ModelMap modelMap)
     {
+        modelMap.addAttribute("produit", new Produit());
         return "createProduit";
     }
     @RequestMapping("/saveProduit")
-    public String saveProduit(@ModelAttribute("produit") Produit produit,
-                              @RequestParam("date") String date,
-                              ModelMap modelMap) throws ParseException
-    {
-        //conversion de la date
-        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateCreation = dateformat.parse(String.valueOf(date));
-        produit.setDateCreation(dateCreation);
+    public String saveProduit(@Valid Produit produit,
+                              BindingResult bindingResult,
+                              ModelMap modelMap) {
+        if (bindingResult.hasErrors()) {
+            return "createProduit";
+        }
         Produit saveProduit = produitService.saveProduit(produit);
-        String msg ="produit enregistré avec Id "+saveProduit.getIdProduit();
+        String msg = "produit enregistré avec Id " +
+                saveProduit.getIdProduit();
         modelMap.addAttribute("msg", msg);
         return "createProduit";
     }
+
     @RequestMapping("/ListeProduits")
     public String listeProduits(
             ModelMap modelMap,@RequestParam(name = "nom", defaultValue = "") String nom,
